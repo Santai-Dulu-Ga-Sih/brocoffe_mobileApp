84d5e5df8dda4dbc62badeb6089a5fe65e9d77cb
yunolva <yunolvazy@gmail.com>
2023-05-25 16:22:36 +0700

    updatefoto

--- lib/pages/kopipedia.dart
+++ lib/pages/kopipedia.dart
@@ -5,77 +5,185 @@ import 'package:brocoffe_moba/main.dart';
 import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(Kopipedia());
 }
 
-class Kopipedia extends StatelessWidget {
+class Kopipedia extends StatefulWidget {
+  @override
+  _KopipediaState createState() => _KopipediaState();
+}
+
+class _KopipediaState extends State<Kopipedia> {
+  String selectedButton = '';
+  bool showDataCard = false;
+  bool showCoffeeCard = false;
+
+  void _handleButtonClick(String buttonName) {
+    setState(() {
+      selectedButton = buttonName;
+      if (buttonName == 'Data') {
+        showDataCard = true;
+        showCoffeeCard = false;
+      } else if (buttonName == 'Coffee') {
+        showDataCard = false;
+        showCoffeeCard = true;
+      } else {
+        showDataCard = false;
+        showCoffeeCard = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(130),
-          child: AppBar(
-            backgroundColor: Colors.white,
-            flexibleSpace: Container(
-              padding: const EdgeInsets.only(left: 55, top: 35),
-              child: Row(
+        backgroundColor: Colors.white,
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 50.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text(
-                        "EZ PZ KOPI",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                          fontFamily: 'Karla',
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        "Nikmati kopi dengan wawasan",
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontFamily: 'Karla',
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        "yang lebih dan mudah",
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontFamily: 'Karla',
-                        ),
-                      ),
-                    ],
+                  CustomButton(
+                    onPressed: () => _handleButtonClick('Data'),
+                    text: 'Data',
+                    selectedButton: selectedButton,
                   ),
-
-                  // Image orang
-                  Column(
-                    children: [
-                      const SizedBox(height: 40),
-                      Container(
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 26.0),
-                          child: Image.asset(
-                            'assets/img/barista.png',
-                            width: 100,
-                          ),
-                        ),
-                      ),
-                    ],
+                  const SizedBox(width: 10.0),
+                  CustomButton(
+                    onPressed: () => _handleButtonClick('Coffee'),
+                    text: 'Coffee',
+                    selectedButton: selectedButton,
                   ),
                 ],
               ),
-            ),
-            leading: IconButton(
-              color: Colors.black,
-              icon: const Icon(Icons.arrow_back_ios_new),
-              onPressed: () => {Navigator.pop(context)},
-            ),
+              const SizedBox(height: 20.0),
+              if (showDataCard) DataCard(),
+              if (showCoffeeCard) CoffeeCard(),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends ElevatedButton {
+  CustomButton({
+    required VoidCallback onPressed,
+    required String text,
+    required String selectedButton,
+    Key? key,
+  }) : super(
+          onPressed: onPressed,
+          key: key,
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>(
+              (states) {
+                if (selectedButton == text) {
+                  return Color.fromARGB(255, 131, 128, 128);
+                }
+                return Colors.white;
+              },
+            ),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+            minimumSize: MaterialStateProperty.all<Size>(
+              Size(120.0, 40.0),
+            ),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: selectedButton == text ? Colors.white : Colors.black,
+            ),
+          ),
+        );
+}
+
+class DataCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide.none, // Menghapus garis
+        borderRadius: BorderRadius.circular(0), // Mengatur sudut kartu
+      ),
+      elevation: 0, // Menghapus elevation
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/img/logo.png',
+            width: 150,
+            height: 150,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Data Card',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Ini adalah deskripsi data card. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CoffeeCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide.none,
+        borderRadius: BorderRadius.circular(0),
+      ),
+      elevation: 0,
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/img/kopi.png',
+            width: 150,
+            height: 150,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Data Card',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Ini adalah deskripsi data card. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
